5da76931aa7ee1f4bd5a121604017fb2c6620fe0
foolcage <5533061@qq.com>
2019-06-26 18:14:43 +0800

    fix travis

--- zvt/init_data_sample.py
+++ zvt/init_data_sample.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from zvt.settings import DATA_SAMPLE_PATH, DATA_SAMPLE_ZIP_PATH
 from zvt.utils.zip_utils import unzip
 
