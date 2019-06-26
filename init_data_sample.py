47a025d4d521cc1e5c6e78f57750ffdba0590efa
foolcage <5533061@qq.com>
2019-06-26 18:28:58 +0800

    fix travis
    Former-commit-id: ee91862adefb4955f84aca52ea9b5627e4ac29ae

--- init_data_sample.py
+++ init_data_sample.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 from zvt.settings import DATA_SAMPLE_PATH, DATA_SAMPLE_ZIP_PATH
 from zvt.utils.zip_utils import unzip
