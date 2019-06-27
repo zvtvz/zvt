8e4256a6c0a68e4e86f0bd691ed40894924cf4b1
foolcage <5533061@qq.com>
2019-06-27 11:48:54 +0800

    fix pypi

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
