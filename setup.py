3722db904d172f73f38168136958e4f80ccf43d2
foolcage <5533061@qq.com>
2019-07-08 15:47:49 +0800

    version 0.0.7

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.6',  # Required
+    version='0.0.7',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
