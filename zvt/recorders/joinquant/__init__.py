2f6a9679f58b48ef98bdf06dec1946e31fc82d0e
foolcage <5533061@qq.com>
2019-09-10 17:08:16 +0800

    1)wechat and email action 2)to_high_level_kdata function

--- zvt/recorders/joinquant/__init__.py
+++ zvt/recorders/joinquant/__init__.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+from zvdata import IntervalLevel
+
+
+def to_jq_trading_level(trading_level: IntervalLevel):
+    if trading_level < IntervalLevel.LEVEL_1HOUR:
+        return trading_level.value
+
+    if trading_level == IntervalLevel.LEVEL_1HOUR:
+        return '60m'
+    if trading_level == IntervalLevel.LEVEL_1DAY:
+        return '1d'
+
+
+def to_jq_entity_id(security_item):
+    if security_item.entity_type == 'stock':
+        if security_item.exchange == 'sh':
+            return '{}.XSHG'.format(security_item.code)
+        if security_item.exchange == 'sz':
+            return '{}.XSHE'.format(security_item.code)
