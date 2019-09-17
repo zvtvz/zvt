0aa3b3fdb93ac2d55d79bbc0b452167078d461f9
foolcage <5533061@qq.com>
2019-09-17 08:38:24 +0800

    fix tests

--- tests/api/test_quote.py
+++ tests/api/test_quote.py
@@ -16,10 +16,10 @@ def test_jq_1mon_kdata():
     df = get_kdata(entity_id='stock_sz_000338', provider='joinquant', level=IntervalLevel.LEVEL_1MON)
     se = df.loc['2010-01-29']
     # make sure our fq is ok
-    assert se['qfq_open'] == 5.51
-    assert se['qfq_high'] == 6.51
-    assert se['qfq_low'] == 5.26
-    assert se['qfq_close'] == 5.52
+    assert round(se['qfq_open'], 2) == 5.51
+    assert round(se['qfq_high'], 2) == 6.51
+    assert round(se['qfq_low'], 2) == 5.26
+    assert round(se['qfq_close'], 2) == 5.52
 
     se = df.loc['2015-06-30']
     assert se['qfq_open'] == 6.63
@@ -32,10 +32,10 @@ def test_jq_1wk_kdata():
     df = get_kdata(entity_id='stock_sz_000338', provider='joinquant', level=IntervalLevel.LEVEL_1WEEK)
     se = df.loc['2019-04-19']
     # make sure our fq is ok
-    assert se['qfq_open'] == 13.45
-    assert se['qfq_high'] == 14.08
-    assert se['qfq_low'] == 12.52
-    assert se['qfq_close'] == 13.47
+    assert round(se['qfq_open'], 2) == 13.45
+    assert round(se['qfq_high'], 2) == 14.08
+    assert round(se['qfq_low'], 2) == 12.52
+    assert round(se['qfq_close'], 2) == 13.47
 
 
 def test_jq_1d_kdata():
@@ -44,5 +44,5 @@ def test_jq_1d_kdata():
     # make sure our fq is ok
     assert round(se['qfq_open'], 2) == 13.08
     assert round(se['qfq_high'], 2) == 13.68
-    assert se['qfq_low'] == 13.04
-    assert se['qfq_close'] == 13.47
+    assert round(se['qfq_low'], 2) == 13.04
+    assert round(se['qfq_close'], 2) == 13.48
