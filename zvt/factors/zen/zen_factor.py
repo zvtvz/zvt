1b1bdb10c9dce24603437d105845e60b388faa16
foolcage <5533061@qq.com>
2019-09-18 17:31:30 +0800

    improve computing performance
    Former-commit-id: e1a6a9c0e0e1334587f6a3309820b9a476eb82c5

--- zvt/factors/zen/zen_factor.py
+++ zvt/factors/zen/zen_factor.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 from zvdata import IntervalLevel
 from zvdata.factor import StateFactor
+from zvt.api import get_securities_in_blocks
 from zvt.factors.technical_factor import TechnicalFactor
-from zvt.settings import SAMPLE_STOCK_CODES
 
 
 class ZenState(Enum):
@@ -45,9 +45,8 @@ class ZenStateFactor(TechnicalFactor, StateFactor):
 
         super().__init__(entity_ids, entity_type, exchanges, codes, the_timestamp, start_timestamp, end_timestamp,
                          columns, filters, order, limit, provider, level, category_field, time_field, trip_timestamp,
-                         auto_load, fq, indicators=['ma', 'ma', 'macd'],
-                         indicators_param=[{'window': short_window}, {'window': long_window},
-                                           {'slow': 26, 'fast': 12, 'n': 9}], valid_window=26)
+                         auto_load, fq, indicators=['ma', 'ma'],
+                         indicators_param=[{'window': short_window}, {'window': long_window}], valid_window=long_window)
 
     def do_compute(self):
         super().do_compute()
@@ -61,16 +60,19 @@ class ZenStateFactor(TechnicalFactor, StateFactor):
             count = 0
             area = 0
             current_state = None
+            pre_index = None
             for index, item in df['score'].iteritems():
                 # ５日线在１０日线之上
                 if item:
-                    col = 'up_count'
-                    col1 = 'up_area'
+                    col_current = 'up_current_count'
+                    col_area = 'up_current_area'
+                    col_total = 'up_total_count'
                     state = 'up'
                 # ５日线在１０日线之下
                 else:
-                    col = 'down_count'
-                    col1 = 'down_area'
+                    col_current = 'down_current_count'
+                    col_area = 'down_current_area'
+                    col_total = 'down_total_count'
                     state = 'down'
 
                 # 计算维持状态的 次数 和相应的 面积
@@ -78,20 +80,36 @@ class ZenStateFactor(TechnicalFactor, StateFactor):
                     count = count + 1
                     area += abs(self.depth_df.loc[index, long_ma_col] - self.depth_df.loc[index, short_ma_col])
                 else:
+                    if count > 0:
+                        self.depth_df.loc[pre_index, col_total] = count
                     current_state = state
                     count = 1
                     area = abs(self.depth_df.loc[index, long_ma_col] - self.depth_df.loc[index, short_ma_col])
 
-                self.depth_df.loc[index, col] = count
-                self.depth_df.loc[index, col1] = area
+                self.depth_df.loc[index, col_current] = count
+                self.depth_df.loc[index, col_area] = area
+
+                pre_index = index
                 # 短期均线　长期均线的距离
-                self.depth_df.loc[index, 'distance'] = self.depth_df.loc[index, short_ma_col] - self.depth_df.loc[
-                    index, long_ma_col]
+                # self.depth_df.loc[index, 'distance'] = abs(self.depth_df.loc[index, short_ma_col] - self.depth_df.loc[
+                #     index, long_ma_col]) / self.depth_df.loc[index, long_ma_col]
 
 
 if __name__ == '__main__':
-    factor = ZenStateFactor(codes=SAMPLE_STOCK_CODES, start_timestamp='2007-01-01', end_timestamp='2019-09-29',
+    entities = get_securities_in_blocks(provider='eastmoney', categories=['concept'], names=['HS300_'])
+    print(entities)
+    factor = ZenStateFactor(entity_ids=entities, start_timestamp='2019-09-10', end_timestamp='2019-09-29',
                             level=IntervalLevel.LEVEL_1WEEK)
+    # factor.depth_df[['ma5',
+    #                  'ma10',
+    #                  'score',
+    #                  'down_current_count',
+    #                  'down_current_area',
+    #                  'up_total_count',
+    #                  'up_current_count',
+    #                  'up_current_area',
+    #                  'down_total_count']
+    # ].to_csv('zen.csv')
+    print(factor.depth_df)
 
-    print(factor.depth_df[['down_count', 'up_count']].groupby('entity_id').describe())
-    print(factor.depth_df[['down_area', 'up_area']].groupby('entity_id').describe())
+    # print(factor.depth_df[['down_total_count', 'up_total_count']].describe())
