c5e80359a58809ff8d76404966d805f97392fe89
foolcage <5533061@qq.com>
2019-10-21 11:40:26 +0800

    get_data default normalize
    Former-commit-id: 542d3949d72a2ca429eea10005e7d23a03ce8005

--- zvt/api/quote.py
+++ zvt/api/quote.py
@@ -74,13 +74,14 @@ def get_securities_in_blocks(provider: str = 'eastmoney',
 
 def get_kdata(entity_id=None, level=IntervalLevel.LEVEL_1DAY.value, provider='eastmoney', columns=None,
               return_type='df', start_timestamp=None, end_timestamp=None,
-              filters=None, session=None, order=None, limit=None):
+              filters=None, session=None, order=None, limit=None, index='timestamp'):
     entity_type, exchange, code = decode_entity_id(entity_id)
     data_schema = get_kdata_schema(entity_type, level=level)
 
     return get_data(data_schema=data_schema, entity_id=entity_id, level=level, provider=provider,
                     columns=columns, return_type=return_type, start_timestamp=start_timestamp,
-                    end_timestamp=end_timestamp, filters=filters, session=session, order=order, limit=limit)
+                    end_timestamp=end_timestamp, filters=filters, session=session, order=order, limit=limit,
+                    index=index)
 
 
 def get_current_price(entity_ids=None, entity_type='coin'):
