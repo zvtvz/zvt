af7c726e86964dc16fd38d2f01f4939c99290a77
foolcage <5533061@qq.com>
2019-10-23 17:45:09 +0800

    add ma factor

---
+++ zvt/domain/factors/stock_1d_ma_factor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Float, String
+from sqlalchemy.ext.declarative import declarative_base
+
+from zvdata import Mixin
+from zvt import register_schema
+
+Stock1dMaFactorBase = declarative_base()
+
+
+class Stock1dMaFactor(Stock1dMaFactorBase, Mixin):
+    __tablename__ = 'Stock1dMaFactor'
+
+    level = Column(String(length=32))
+    ma5 = Column(Float)
+    ma10 = Column(Float)
+
+    ma34 = Column(Float)
+    ma55 = Column(Float)
+    ma89 = Column(Float)
+    ma144 = Column(Float)
+
+    ma120 = Column(Float)
+    ma250 = Column(Float)
+
+
+register_schema(providers=['zvt'], db_name='stock_1d_ma_factor', schema_base=Stock1dMaFactorBase)
