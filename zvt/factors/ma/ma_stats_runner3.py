ba8779809aac6a11aa8b65ccb9f299cb3a15eb37
foolcage <5533061@qq.com>
2019-11-05 12:13:43 +0800

    fix ma runner
    Former-commit-id: 38372f01c04cd3ea6158ae771048633acd466dcd

--- zvt/factors/ma/ma_stats_runner3.py
+++ zvt/factors/ma/ma_stats_runner3.py
@@ -4,6 +4,7 @@ import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from zvt import init_log
+from zvt.factors.ma.common import cal_ma_states
 
 logger = logging.getLogger(__name__)
 
@@ -11,14 +12,14 @@ sched = BackgroundScheduler()
 
 
 @sched.scheduled_job('cron', hour=18, minute=0)
-def cal_ma_states():
+def run():
     cal_ma_states(start='3000000', end='600000')
 
 
 if __name__ == '__main__':
     init_log('ma_stats_runner3.log')
 
-    cal_ma_states()
+    run()
 
     sched.start()
 
