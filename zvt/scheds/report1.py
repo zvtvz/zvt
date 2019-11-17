0d64e062f311c5540b96cb99440ebfd37784b0f2
foolcage <5533061@qq.com>
2019-11-17 18:42:00 +0800

    fix select day to today
    Former-commit-id: 448ec510ce2af3ec26878d0ecdd37bdd2c5fcf4e

--- zvt/scheds/report1.py
+++ zvt/scheds/report1.py
@@ -5,7 +5,7 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from zvdata import IntervalLevel
-from zvdata.utils.time_utils import now_pd_timestamp
+from zvdata.utils.time_utils import now_pd_timestamp, to_time_str
 from zvt import init_log
 from zvt.factors.ma.ma_factor import CrossMaFactor
 from zvt.factors.target_selector import TargetSelector
@@ -26,22 +26,24 @@ def every_day_report():
             ChinaStockKdataRecorder(level=IntervalLevel.LEVEL_1WEEK).run()
             ChinaStockKdataRecorder(level=IntervalLevel.LEVEL_1MON).run()
 
+            today = to_time_str(now_pd_timestamp())
+
             # 计算均线
-            my_selector = TargetSelector(start_timestamp='2017-01-01', end_timestamp=now_pd_timestamp())
+            my_selector = TargetSelector(start_timestamp='2017-01-01', end_timestamp=today)
             # add the factors
             # 设置dry_run为True，因为我们只需要最近的数据，不需要加载全量数据
-            ma_factor = CrossMaFactor(start_timestamp='2017-01-01', end_timestamp=now_pd_timestamp(), dry_run=True)
+            ma_factor = CrossMaFactor(start_timestamp='2017-01-01', end_timestamp=today, dry_run=True)
 
             my_selector.add_filter_factor(ma_factor)
 
             my_selector.run()
 
-            long_targets = my_selector.get_open_long_targets(timestamp='2019-11-11')
+            long_targets = my_selector.get_open_long_targets(timestamp=today)
             print(long_targets)
 
             email_action = EmailInformer()
             msg = '\n'.join(long_targets)
-            email_action.send_message("5533061@qq.com", msg, f'{now_pd_timestamp()} 选股结果')
+            email_action.send_message("5533061@qq.com", msg, f'{today} 选股结果')
 
             break
         except Exception as e:
