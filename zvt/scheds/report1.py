c42b0005db5413adf76be7cbe3cdf8adec68e028
foolcage <5533061@qq.com>
2019-11-19 13:43:23 +0800

    better report
    Former-commit-id: c6efa4f06698ecf576add11241f027be6d6e3b3b

--- zvt/scheds/report1.py
+++ zvt/scheds/report1.py
@@ -5,8 +5,10 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from zvdata import IntervalLevel
+from zvdata.api import get_entities
 from zvdata.utils.time_utils import now_pd_timestamp, to_time_str
 from zvt import init_log
+from zvt.domain import Stock
 from zvt.factors.ma.ma_factor import CrossMaFactor
 from zvt.factors.target_selector import TargetSelector
 from zvt.informer.informer import EmailInformer
@@ -21,17 +23,21 @@ sched = BackgroundScheduler()
 def every_day_report():
     while True:
         try:
+            t = now_pd_timestamp()
+            if t.dayofweek in (5, 6):
+                logger.info(f'today:{t} is {t.day_name()},just ignore')
+
+            today = to_time_str(t)
+
             # 抓取k线数据
             ChinaStockKdataRecorder(level=IntervalLevel.LEVEL_1DAY).run()
             ChinaStockKdataRecorder(level=IntervalLevel.LEVEL_1WEEK).run()
             ChinaStockKdataRecorder(level=IntervalLevel.LEVEL_1MON).run()
 
-            today = to_time_str(now_pd_timestamp())
-
             # 计算均线
             my_selector = TargetSelector(start_timestamp='2017-01-01', end_timestamp=today)
             # add the factors
-            # 设置dry_run为True，因为我们只需要最近的数据，不需要加载全量数据
+            # 设置dry_run为True，因为我们只需要最近的数据，不需要加载全量数据进行回测
             ma_factor = CrossMaFactor(start_timestamp='2017-01-01', end_timestamp=today, dry_run=True)
 
             my_selector.add_filter_factor(ma_factor)
@@ -39,10 +45,15 @@ def every_day_report():
             my_selector.run()
 
             long_targets = my_selector.get_open_long_targets(timestamp=today)
-            print(long_targets)
+            if long_targets:
+                df = get_entities(provider='eastmoney', entity_schema=Stock, entity_ids=long_targets,
+                                  columns=['code', 'name'])
+                info = [df.loc[i, 'code'] + ' ' + df.loc[i, 'name'] for i in df.index]
+                msg = '\n'.join(info)
+            else:
+                msg = 'no targets'
 
             email_action = EmailInformer()
-            msg = '\n'.join(long_targets)
             email_action.send_message("5533061@qq.com", f'{today} 选股结果', msg)
 
             break
