189f45b45b01aa1e647eba9ec92c227f946e1702
foolcage <5533061@qq.com>
2020-01-15 12:58:25 +0800

    improve record_data

--- tests/trader/test_trader.py
+++ tests/trader/test_trader.py
@@ -6,6 +6,6 @@ from zvt.samples import MyMaTrader
 def test_basic_trader():
     try:
         MyMaTrader(codes=['000338'], level=IntervalLevel.LEVEL_1DAY, start_timestamp='2018-01-01',
-                   end_timestamp='2019-06-30', trader_name='000338_ma_trader').run()
+                   end_timestamp='2019-06-30', trader_name='000338_ma_trader', draw_result=False).run()
     except:
         assert False
