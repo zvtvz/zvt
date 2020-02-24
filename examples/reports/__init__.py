87964fac5d50fd52752048a0cc60431875f55e1f
Evergreen2020 <61366517+Evergreen2020@users.noreply.github.com>
2020-02-24 13:38:34 +0800

    Update_Init_.py

--- examples/reports/__init__.py
+++ examples/reports/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 subscriber_emails = ['2242535441@qq.com', '2315983623@qq.com', '31591084@qq.com', '348886500@qq.com',
                      '359101562@qq.com', '5533061@qq.com', 'dragonllt_usdt@163.com', 'laosiji@protonmail.com',
-                     'manstiilin@protonmail.com']
+                     'manstiilin@protonmail.com','786538768@qq.com']
