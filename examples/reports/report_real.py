48984ee1c08d24a1be9ad24e3b823de26a8dfc63
foolcage <5533061@qq.com>
2020-03-14 21:26:55 +0800

    update real report

--- examples/reports/report_real.py
+++ examples/reports/report_real.py
@@ -7,11 +7,10 @@ import eastmoneypy
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from examples.factors.block_selector import BlockSelector
-from examples.reports import get_subscriber_emails
 from zvt import init_log
 from zvt.domain import Stock1dKdata, BlockStock
+from zvt.factors import TargetSelector
 from zvt.factors.ma.ma_factor import VolumeUpMa250Factor
-from zvt.factors.target_selector import TargetSelector
 from zvt.informer.informer import EmailInformer
 
 logger = logging.getLogger(__name__)
@@ -26,10 +25,6 @@ def report_real():
         email_action = EmailInformer()
 
         try:
-            # 抓取k线数据
-            # StockTradeDay.record_data(provider='joinquant')
-            # Stock1dKdata.record_data(provider='joinquant')
-
             latest_day: Stock1dKdata = Stock1dKdata.query_data(order=Stock1dKdata.timestamp.desc(), limit=1,
                                                                return_type='domain')
             target_date = latest_day[0].timestamp
@@ -43,37 +38,49 @@ def report_real():
 
             my_selector.run()
 
-            long_targets = my_selector.get_open_long_targets(timestamp=target_date)
-            if long_targets:
+            long_stocks = my_selector.get_open_long_targets(timestamp=target_date)
+
+            msg = 'no targets'
+            if long_stocks:
                 # use block to filter
                 block_selector = BlockSelector(start_timestamp='2020-01-01')
                 block_selector.run()
                 long_blocks = block_selector.get_open_long_targets(timestamp=target_date)
-                block_stocks: List[BlockStock] = BlockStock.query_data(provider='sina',
-                                                                       filters=[BlockStock.stock_id.in_(long_targets)],
-                                                                       entity_ids=long_blocks, return_type='domain')
-                # add them to eastmoney
-                try:
-                    try:
-                        eastmoneypy.del_group('real')
-                    except:
-                        pass
-                    eastmoneypy.create_group('real')
-                    for stock in block_stocks:
-                        eastmoneypy.add_to_group(stock.stock_code, group_name='real')
-                except Exception as e:
-                    email_action.send_message("5533061@qq.com", f'report_real error',
-                                              'report_real error:{}'.format(e))
 
-                info = [f'{stock.stock_name}({stock.stock_code})' for stock in block_stocks]
-                msg = ' '.join(info)
-            else:
-                msg = 'no targets'
+                if long_blocks:
+                    block_stocks: List[BlockStock] = BlockStock.query_data(provider='sina',
+                                                                           filters=[
+                                                                               BlockStock.stock_id.in_(long_stocks)],
+                                                                           entity_ids=long_blocks, return_type='domain')
+                    if block_stocks:
+                        # add them to eastmoney
+                        try:
+                            try:
+                                eastmoneypy.del_group('real')
+                            except:
+                                pass
+                            eastmoneypy.create_group('real')
+                            for block_stock in block_stocks:
+                                eastmoneypy.add_to_group(block_stock.stock_code, group_name='real')
+                        except Exception as e:
+                            email_action.send_message("5533061@qq.com", f'report_real error',
+                                                      'report_real error:{}'.format(e))
 
-            logger.info(msg)
+                        block_map_stocks = {}
+                        for block_stock in block_stocks:
+                            stocks = block_map_stocks.get(block_stock.name)
+                            if not stocks:
+                                stocks = []
+                                block_stocks[block_stock.name] = stocks
+                            stocks.append(f'{block_stock.stock_name}({block_stock.stock_code})')
 
-            email_action.send_message(get_subscriber_emails(), f'{target_date} 放量突破年线real选股结果', msg)
+                        msg = ''
+                        for block, stocks in block_map_stocks:
+                            info = [f'{stock.name}({stock.code})' for stock in stocks]
+                            stock_msg = ' '.join(info)
+                            msg = msg + f'{block}:\n' + stock_msg + '\n'
 
+            email_action.send_message('5533061@qq.com', f'{target_date} 放量突破年线real选股结果', msg)
             break
         except Exception as e:
             logger.exception('report_real error:{}'.format(e))
