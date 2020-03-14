aa45231add9091598e9d589e5a1f86419277e757
foolcage <5533061@qq.com>
2020-03-14 22:10:31 +0800

    fix bug
    Former-commit-id: 110672eb946dd3a4a8edaae3a76c1eb358f7a9c9

--- examples/reports/report_real.py
+++ examples/reports/report_real.py
@@ -28,6 +28,7 @@ def report_real():
             latest_day: Stock1dKdata = Stock1dKdata.query_data(order=Stock1dKdata.timestamp.desc(), limit=1,
                                                                return_type='domain')
             target_date = latest_day[0].timestamp
+            # target_date = '2020-02-04'
 
             # 计算均线
             my_selector = TargetSelector(start_timestamp='2018-01-01', end_timestamp=target_date)
@@ -77,8 +78,7 @@ def report_real():
                         msg = ''
                         for block in block_map_stocks:
                             stocks = block_map_stocks[block]
-                            info = [f'{stock.name}({stock.code})' for stock in stocks]
-                            stock_msg = ' '.join(info)
+                            stock_msg = ' '.join(stocks)
                             msg = msg + f'{block}:\n' + stock_msg + '\n'
 
             email_action.send_message('5533061@qq.com', f'{target_date} 放量突破年线real选股结果', msg)
