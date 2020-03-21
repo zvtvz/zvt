f666992ba1cb18950689f4e31cbd43bc0e4ca946
foolcage <5533061@qq.com>
2020-03-22 00:01:21 +0800

    risky company examples
    Former-commit-id: 810d4a72b93c152bd581e649fb9f505a3f8182f3

--- examples/reports/__init__.py
+++ examples/reports/__init__.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
+import datetime
 import json
 import os
+
+from sqlalchemy import or_
+
+from zvdata.utils.time_utils import to_pd_timestamp
+from zvt.domain import FinanceFactor
 
 
 def get_subscriber_emails():
     emails_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'subscriber_emails.json'))
     with open(emails_file) as f:
         return json.load(f)
+
+
+def danger_company(the_date):
+    start_timestamp = to_pd_timestamp(the_date) - datetime.timedelta(190)
+    # 营收降，利润降,流动比率低，速动比率低
+    danger_filter = or_(FinanceFactor.op_income_growth_yoy < -0.2,
+                        FinanceFactor.net_profit_growth_yoy <= -0.3,
+                        FinanceFactor.current_ratio < 0.7,
+                        FinanceFactor.quick_ratio < 0.5)
+    FinanceFactor.query_data(start_timestamp='2019-09-30', filters=[danger_filter],
+                             columns=FinanceFactor.important_cols())
 
 
 if __name__ == '__main__':
