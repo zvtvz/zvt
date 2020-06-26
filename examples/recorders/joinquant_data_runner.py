52355476e3e88bff698e7ee700a8579b6a9311ca
foolcage <5533061@qq.com>
2020-06-26 22:47:25 +0800

    update recorder examples

--- examples/recorders/joinquant_data_runner.py
+++ examples/recorders/joinquant_data_runner.py
@@ -5,8 +5,8 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from zvt import init_log
-from zvt.informer.informer import EmailInformer
 from zvt.domain import *
+from zvt.informer.informer import EmailInformer
 
 logger = logging.getLogger(__name__)
 
@@ -21,8 +21,13 @@ def record_kdata():
         try:
             Stock.record_data(provider='joinquant', sleeping_time=1)
             StockTradeDay.record_data(provider='joinquant', sleeping_time=1)
+            # 日线前复权和后复权数据
             Stock1dKdata.record_data(provider='joinquant', sleeping_time=1)
             Stock1dHfqKdata.record_data(provider='joinquant', sleeping_time=1)
+            # 周线前复权和后复权数据
+            Stock1wkKdata.record_data(provider='joinquant', sleeping_time=1)
+            Stock1wkHfqKdata.record_data(provider='joinquant', sleeping_time=1)
+            # 个股估值数据
             StockValuation.record_data(provider='joinquant', sleeping_time=1)
 
             email_action.send_message("5533061@qq.com", 'joinquant record kdata finished', '')
@@ -35,7 +40,7 @@ def record_kdata():
             time.sleep(60)
 
 
-@sched.scheduled_job('cron', hour=19, minute=00)
+@sched.scheduled_job('cron', hour=19, minute=00, day_of_week=3)
 def record_others():
     while True:
         email_action = EmailInformer()
