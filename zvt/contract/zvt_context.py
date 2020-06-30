5cfbe40ee0a11e1f16036354344342fa615834e0
邱承 <qiucheng1@sina.com>
2020-06-30 18:07:24 +0800

    Support mysql as db engine

--- zvt/contract/zvt_context.py
+++ zvt/contract/zvt_context.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import create_engine
+from zvt import zvt_env
 
 # all registered providers
 providers = []
@@ -15,59 +16,63 @@ entity_schema_map = {}
 
 # global sessions
 sessions = {}
-mysqlsql_engine = create_engine("mysql://zvt:jinniuzvt321@123.103.74.231/zvt", echo=False)
 
-# provider_dbname -> engine
-db_engine_map = {
-    "joinquant_stock_meta": mysqlsql_engine,
-"eastmoney_block_1d_kdata": mysqlsql_engine,
-"joinquant_stock_1mon_kdata": mysqlsql_engine,
-"eastmoney_block_1mon_kdata": mysqlsql_engine,
-"joinquant_stock_1wk_hfq_kdata": mysqlsql_engine,
-"eastmoney_block_1wk_kdata": mysqlsql_engine,
-"joinquant_stock_1wk_kdata": mysqlsql_engine,
-"eastmoney_dividend_financing": mysqlsql_engine,
-"joinquant_stock_30m_hfq_kdata": mysqlsql_engine,
-"eastmoney_finance": mysqlsql_engine,
-"joinquant_stock_30m_kdata": mysqlsql_engine,
-"eastmoney_holder": mysqlsql_engine,
-"joinquant_stock_4h_hfq_kdata": mysqlsql_engine,
-"eastmoney_stock_meta": mysqlsql_engine,
-"joinquant_stock_4h_kdata": mysqlsql_engine,
-"eastmoney_trading": mysqlsql_engine,
-"joinquant_stock_5m_hfq_kdata": mysqlsql_engine,
-"exchange_overall": mysqlsql_engine,
-"joinquant_stock_5m_kdata": mysqlsql_engine,
-"exchange_stock_meta": mysqlsql_engine,
-"joinquant_trade_day": mysqlsql_engine,
-"joinquant_overall": mysqlsql_engine,
-"joinquant_valuation": mysqlsql_engine,
-"joinquant_stock_15m_hfq_kdata":mysqlsql_engine,
-"sina_etf_1d_kdata": mysqlsql_engine,
-"joinquant_stock_15m_kdata": mysqlsql_engine,
-"sina_index_1d_kdata": mysqlsql_engine,
-"joinquant_stock_1d_hfq_kdata": mysqlsql_engine,
-"sina_money_flow": mysqlsql_engine,
-"joinquant_stock_1d_kdata": mysqlsql_engine,
-"sina_stock_meta": mysqlsql_engine,
-"joinquant_stock_1h_hfq_kdata": mysqlsql_engine,
-"zvt_stock_1d_ma_factor": mysqlsql_engine,
-"joinquant_stock_1h_kdata": mysqlsql_engine,
-"zvt_stock_1d_ma_stats": mysqlsql_engine,
-"joinquant_stock_1m_hfq_kdata": mysqlsql_engine,
-"zvt_stock_1d_zen_factor": mysqlsql_engine,
-"joinquant_stock_1m_kdata": mysqlsql_engine,
-"zvt_stock_1wk_ma_stats": mysqlsql_engine,
-"joinquant_stock_1mon_hfq_kdata": mysqlsql_engine,
-"zvt_trader_info": mysqlsql_engine,
-}
+if zvt_env['db_engine'] and zvt_env['db_engine'] == "mysql":
+    mysqlsql_engine = create_engine("mysql://zvt:jinniuzvt321@123.103.74.231/zvt", echo=False)
+
+    # provider_dbname -> engine
+    db_engine_map = {
+        "joinquant_stock_meta": mysqlsql_engine,
+        "eastmoney_block_1d_kdata": mysqlsql_engine,
+        "joinquant_stock_1mon_kdata": mysqlsql_engine,
+        "eastmoney_block_1mon_kdata": mysqlsql_engine,
+        "joinquant_stock_1wk_hfq_kdata": mysqlsql_engine,
+        "eastmoney_block_1wk_kdata": mysqlsql_engine,
+        "joinquant_stock_1wk_kdata": mysqlsql_engine,
+        "eastmoney_dividend_financing": mysqlsql_engine,
+        "joinquant_stock_30m_hfq_kdata": mysqlsql_engine,
+        "eastmoney_finance": mysqlsql_engine,
+        "joinquant_stock_30m_kdata": mysqlsql_engine,
+        "eastmoney_holder": mysqlsql_engine,
+        "joinquant_stock_4h_hfq_kdata": mysqlsql_engine,
+        "eastmoney_stock_meta": mysqlsql_engine,
+        "joinquant_stock_4h_kdata": mysqlsql_engine,
+        "eastmoney_trading": mysqlsql_engine,
+        "joinquant_stock_5m_hfq_kdata": mysqlsql_engine,
+        "exchange_overall": mysqlsql_engine,
+        "joinquant_stock_5m_kdata": mysqlsql_engine,
+        "exchange_stock_meta": mysqlsql_engine,
+        "joinquant_trade_day": mysqlsql_engine,
+        "joinquant_overall": mysqlsql_engine,
+        "joinquant_valuation": mysqlsql_engine,
+        "joinquant_stock_15m_hfq_kdata": mysqlsql_engine,
+        "sina_etf_1d_kdata": mysqlsql_engine,
+        "joinquant_stock_15m_kdata": mysqlsql_engine,
+        "sina_index_1d_kdata": mysqlsql_engine,
+        "joinquant_stock_1d_hfq_kdata": mysqlsql_engine,
+        "sina_money_flow": mysqlsql_engine,
+        "joinquant_stock_1d_kdata": mysqlsql_engine,
+        "sina_stock_meta": mysqlsql_engine,
+        "joinquant_stock_1h_hfq_kdata": mysqlsql_engine,
+        "zvt_stock_1d_ma_factor": mysqlsql_engine,
+        "joinquant_stock_1h_kdata": mysqlsql_engine,
+        "zvt_stock_1d_ma_stats": mysqlsql_engine,
+        "joinquant_stock_1m_hfq_kdata": mysqlsql_engine,
+        "zvt_stock_1d_zen_factor": mysqlsql_engine,
+        "joinquant_stock_1m_kdata": mysqlsql_engine,
+        "zvt_stock_1wk_ma_stats": mysqlsql_engine,
+        "joinquant_stock_1mon_hfq_kdata": mysqlsql_engine,
+        "zvt_trader_info": mysqlsql_engine,
+    }
+else:
+    db_engine_map = {}
 
 # provider_dbname -> session
 db_session_map = {}
 
 # provider -> [db_name1,db_name2...]
 provider_map_dbnames = {
-    "joinquant":["zvt"]
+    "joinquant": ["zvt"]
 
 }
 
