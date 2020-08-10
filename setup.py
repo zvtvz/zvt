4c62ddaa21153e66f545a2fc40d6215e5eb1dc87
foolcage <5533061@qq.com>
2020-08-10 22:30:43 +0800

    release 0.8.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except:
 
 setup(
     name='zvt',
-    version='0.8.5',
+    version='0.8.6',
     description='unified,modular quantitative system for human beings ',
     long_description=long_description,
     url='https://github.com/zvtvz/zvt',
