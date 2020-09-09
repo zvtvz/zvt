c5ede413517f5f64da6033be05b8c2bf202591eb
wlwd13303 <327714319@qq.com>
2020-09-09 11:28:34 +0800

    UPDATE：删除错误表

--- zvt/recorders/joinquant/finance_pit/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from zvt.recorders.joinquant.finance.jq_stock_balance_sheet_recorder import *
-from zvt.recorders.joinquant.finance.jq_stock_cash_flow_recorder import *
-from zvt.recorders.joinquant.finance.jq_stock_finance_factor_recorder import *
-from zvt.recorders.joinquant.finance.jq_stock_income_statement_recorder import *
