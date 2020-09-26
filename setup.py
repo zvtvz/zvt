19360b3f29992bc759709adfa90e32843147a807
foolcage <5533061@qq.com>
2020-09-26 16:52:32 +0800

    release 0.8.9

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except:
 
 setup(
     name='zvt',
-    version='0.8.8',
+    version='0.8.9',
     description='unified,modular quantitative system for human beings ',
     long_description=long_description,
     url='https://github.com/zvtvz/zvt',
