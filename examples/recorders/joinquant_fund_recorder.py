9bbe3dde536a96948e5c06ecb3501d22ceeac7f1
foolcage <5533061@qq.com>
2021-01-20 08:35:52 +0800

    get top fund holding stocks by pct

--- examples/recorders/joinquant_fund_recorder.py
+++ examples/recorders/joinquant_fund_recorder.py
@@ -5,7 +5,7 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from zvt import init_log
-from zvt.domain import Fund, FundStock, Stock1wkHfqKdata
+from zvt.domain import Fund, FundStock, Stock1wkHfqKdata, StockValuation
 from zvt.informer.informer import EmailInformer
 
 logger = logging.getLogger(__name__)
@@ -36,8 +36,29 @@ def record_fund():
             time.sleep(60)
 
 
+# 周2抓取
+@sched.scheduled_job('cron', hour=19, minute=00, day_of_week=1)
+def record_valuation():
+    while True:
+        email_action = EmailInformer()
+
+        try:
+            StockValuation.record_data(provider='joinquant', sleeping_time=0)
+
+            email_action.send_message("5533061@qq.com", 'joinquant record valuation finished', '')
+            break
+        except Exception as e:
+            msg = f'joinquant record valuation error:{e}'
+            logger.exception(msg)
+
+            email_action.send_message("5533061@qq.com", 'joinquant record valuation error', msg)
+            time.sleep(60)
+
+
 if __name__ == '__main__':
     init_log('joinquant_fund_runner.log')
+
+    record_valuation()
 
     record_fund()
 
