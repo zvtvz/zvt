3c1fdbb1c579225a59567ff211001f3c2c16a915
foolcage <5533061@qq.com>
2021-10-06 23:58:30 +0800

    release v0.9.8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except:
 
 setup(
     name='zvt',
-    version='0.9.7',
+    version='0.9.8',
     description='unified,modular quant framework for human beings ',
     long_description=long_description,
     url='https://github.com/zvtvz/zvt',
