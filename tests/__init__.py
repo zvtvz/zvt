054bf8a3e7a049df7087c324fa87e8effbaf5bdc
foolcage <5533061@qq.com>
2021-12-27 01:09:03 +0800

    fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 
 os.environ.setdefault("TESTING_ZVT", "True")
 os.environ.setdefault("SQLALCHEMY_WARN_20", "1")
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
--- tests/context.py
+++ tests/context.py
@@ -5,4 +5,4 @@ def init_test_context():
     import os
     import sys
 
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
