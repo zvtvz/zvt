c5daeb3dc394305d3eb7298f5ff0e01cbd237760
foolcage <5533061@qq.com>
2022-05-11 00:26:11 +0800

    release v0.10.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, foolcage"
 author = "foolcage"
 
 # The full version, including alpha/beta/rc tags
-release = "0.10.0"
+release = "0.10.1`"
 
 
 # -- General configuration ---------------------------------------------------
