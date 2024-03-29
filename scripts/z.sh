c2d462f3ad25b7c9f0dac32c39f92100799dfbb8
foolcage <5533061@qq.com>
2024-03-29 12:43:33 +0800

    Add a.json to .gitignore

---
+++ scripts/z.sh
@@ -0,0 +1,3 @@
+nohup python examples/data_runner/kdata_runner.py >/dev/null 2>&1 &
+nohup python examples/reports/report_tops.py >/dev/null 2>&1 &
+nohup python examples/reports/report_vol_up.py >/dev/null 2>&1 &
