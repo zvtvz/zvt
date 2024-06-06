28137505c3711fb153975dff3c7436df66decde9
foolcage <5533061@qq.com>
2024-06-06 23:26:26 +0800

    Fix package dep

--- src/zvt/contract/register.py
+++ src/zvt/contract/register.py
@@ -124,33 +124,33 @@ def register_schema(
                             )
                             logger.info(f"{engine.url} migrations:\n {stmt}")
                             con.execute(stmt)
+
+                    logger.debug("engine:{},table:{},index:{}".format(engine, table_name, index_list))
+
+                    for col in [
+                        "timestamp",
+                        "entity_id",
+                        "code",
+                        "report_period",
+                        "created_timestamp",
+                        "updated_timestamp",
+                    ]:
+                        if col in table.c:
+                            column = eval("table.c.{}".format(col))
+                            index_name = "{}_{}_index".format(table_name, col)
+                            if index_name not in index_list:
+                                index = sqlalchemy.schema.Index(index_name, column)
+                                index.create(engine)
+                    for cols in [("timestamp", "entity_id"), ("timestamp", "code")]:
+                        if (cols[0] in table.c) and (col[1] in table.c):
+                            column0 = eval("table.c.{}".format(col[0]))
+                            column1 = eval("table.c.{}".format(col[1]))
+                            index_name = "{}_{}_{}_index".format(table_name, col[0], col[1])
+                            if index_name not in index_list:
+                                index = sqlalchemy.schema.Index(index_name, column0, column1)
+                                index.create(engine)
                 except Exception as e:
                     logger.error(e)
-
-            logger.debug("engine:{},table:{},index:{}".format(engine, table_name, index_list))
-
-            for col in [
-                "timestamp",
-                "entity_id",
-                "code",
-                "report_period",
-                "created_timestamp",
-                "updated_timestamp",
-            ]:
-                if col in table.c:
-                    column = eval("table.c.{}".format(col))
-                    index_name = "{}_{}_index".format(table_name, col)
-                    if index_name not in index_list:
-                        index = sqlalchemy.schema.Index(index_name, column)
-                        index.create(engine)
-            for cols in [("timestamp", "entity_id"), ("timestamp", "code")]:
-                if (cols[0] in table.c) and (col[1] in table.c):
-                    column0 = eval("table.c.{}".format(col[0]))
-                    column1 = eval("table.c.{}".format(col[1]))
-                    index_name = "{}_{}_{}_index".format(table_name, col[0], col[1])
-                    if index_name not in index_list:
-                        index = sqlalchemy.schema.Index(index_name, column0, column1)
-                        index.create(engine)
 
 
 # the __all__ is generated
