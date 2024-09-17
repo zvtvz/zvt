f2848e095b78a1e462068fc32590a2e4ca51ed9c
foolcage <5533061@qq.com>
2024-09-17 21:35:30 +0800

    Release 0.12.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="zvt",
-    version="0.12.1",
+    version="0.12.2",
     description="unified,modular quant framework for human beings ",
     long_description=long_description,
     url="https://github.com/zvtvz/zvt",
