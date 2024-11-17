d20f6813c49a5650b899fbb306a78a9d7ca03ac3
foolcage <5533061@qq.com>
2024-11-17 17:55:17 +0800

    Release 0.13.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, foolcage"
 author = "foolcage"
 
 # The full version, including alpha/beta/rc tags
-release = "0.10.5"
+release = "0.13.0"
 
 
 # -- General configuration ---------------------------------------------------
